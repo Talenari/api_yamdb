f9dc88fdadf03725a8fed919f358253a4c6d402e
Дмитрий KhahaDu <dmitri.khaydukov@gmail.com>
2023-05-31 02:32:28 +0500

    add user registration and email-emulation

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from rest_framework.serializers import ModelSerializer
 
 from .models import User
@@ -7,3 +8,17 @@ class UserSignupSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+    def create(self, validated_data):
+        password = User.objects.make_random_password(length=32)
+        send_mail(
+            'Код подтверждения',
+            f'Ваш код подтверждения {password}.',
+            'from@example.com',
+            ['to@example.com'],
+        )
+        return User.objects.create(
+            username=validated_data.get('username'),
+            email=validated_data.get('email'),
+            password=password
+        )
