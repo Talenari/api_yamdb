af9fc758bf3cc06f6e54cae1c6305628735997b2
Alena <Talenary@gmail.com>
2023-06-10 20:17:40 +0500

    beautify

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
 
-from users.models import User
 
 class CreatorOnlyPermission(permissions.BasePermission):
     """Пермишн для содателя объекта, модератора, администратора."""
